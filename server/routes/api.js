72d04729763273950d641430b93fbde67b77eeed
stremann <stremann@yahoo.com>
2015-03-21 22:06:04 +0200

    Add comments.json

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,14 +1,36 @@
 module.exports = function(app) {
-  var comments = [
-    { "author": "Roman Stremedlovskyi", "text": "This is REACT" }
-  ];
+
+  var fs = require('fs');
+  var path = require('path');
+
+  var filePath = path.join(__dirname, '../model/comments.json');
 
   app.get('/api/comments', function(req, res) {
-    res.json(comments);
+
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data) {
+        if (!err) {
+          res.writeHead(200, {'Content-Type': 'text/json'});
+          res.write(data);
+          res.end();
+        } else {
+            console.log(err);
+        }
+
+    });
+
   });
 
   app.post('/api/comments', function(req, res) {
-    comments.push(req.body);
-    res.json(comments);
+
+    fs.readFile(filePath, function(err, data) {
+      var comments = JSON.parse(data);
+      comments.push(req.body);
+      fs.writeFile(filePath, JSON.stringify(comments, null, 4), function(err) {
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Cache-Control', 'no-cache');
+        res.send(JSON.stringify(comments));
+      });
+    });
+
   });
 };
